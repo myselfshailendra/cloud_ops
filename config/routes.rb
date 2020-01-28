899020544ac5509703be42c732b20cdc5d2c8163
Shailendra Prajapati <myselfshailendra@hotmail.com>
2020-01-28 16:12:09 +0530

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'service/:service_code/region/:location' => 'regions#show', as: :show_region_prices
+      get 'service/:service_code/region/:location', to: 'regions#show_region_prices', as: :show_region_prices
     end
   end
 end
--- spec/routing/regions_routes_spec.rb
+++ spec/routing/regions_routes_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'RegionsRoutes', type: :routing do
-  describe 'GET #show' do
+  describe 'GET #show_region_prices' do
     it { expect(get: 'api/v1/service/AmazonCloudFront/region/us-east-1').to route_to(controller: 'api/v1/regions',
-                                                                                     action: 'show',
+                                                                                     action: 'show_region_prices',
                                                                                      service_code: 'AmazonCloudFront',
                                                                                      location: 'us-east-1') }
   end
